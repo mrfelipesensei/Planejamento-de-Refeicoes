2a6d717ad22447dfcfe6ae05d43d3e3b31240984
Felipe Greg <mrfelipesensei@gmail.com>
2025-03-15 15:58:56 -0300

    app.py v 1.0

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+from flask import Flask, render_template, request
 import requests
 
 api_key = '174ea246a9bc4c4f9f9b862a356ac383'
+
+app = Flask(__name__)
 
 #Função para buscar receitas
 def buscar_receitas(dieta,numero_de_receitas=5):
@@ -12,21 +15,34 @@ def buscar_receitas(dieta,numero_de_receitas=5):
     if response.status_code == 200:
         return response.json() #Retorna os dados no formato JSON
     else:
-        print(f"Erro: {response.status_code}")
         return None
     
-#Exemplo de uso
-dieta = 'vegan'
-receitas = buscar_receitas(dieta)
+#Página inicial
+def index():
+    return render_template('index.html')
 
-if receitas:
-    for i, receita in enumerate(receitas['results'],start=1):
-        print(f"{i}. {receita['title']}")
-        
-        if 'sourceUrl' in receita:
-            print(f"Link: {receita['sourceUrl']}")
-        else:
-            receita_id = receita['id']
-            print(f"Link alternativo: https://spoonacular.com/recipes/{receita['title'].replace(' ', '-')}-{receita_id}")
+#Página para buscar receitas
+@app.route('/buscar',methods=['POST'])
+def buscar():
+    dieta = request.form['dieta']
+    numero_de_receitas = int(request.form['numero_de_receitas'])
+    receitas = buscar_receitas(dieta, numero_de_receitas)
 
-        print("-" * 50)
+    if receitas:
+        receitas_list = []
+        for receita in receitas['results']:
+            link = ""
+            if 'sourceUrl' in receita:
+                link = receita['sourceUrl']
+            else:
+                receita_id = receita['id']
+                link = f"https://spoonacular.com/recipes/{receita['title'].replace('','-')}--{receita_id}"
+
+            receitas_list.append({'title':receita['title'],'link':link})
+
+        return render_template('index.html',receitas=receitas_list)
+    else:
+        return render_template('index.html',error="Nenhuma receita encontrada.")
+    
+if __name__ == '__main__':
+    app.run(debug=True)
